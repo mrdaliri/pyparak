a0b02ab0c9c312faa828be27252a5cd407555783
AmirHosseinFayyazian <amirh.fayyazian95@gmail.com>
2019-05-16 02:12:29 +0430

    added ZarinpalGateway

--- pyparak/__init__.py
+++ pyparak/__init__.py
@@ -1,3 +1,7 @@
+from zeep import Client
+
+zarinpal_client = Client('https://sandbox.zarinpal.com/pg/services/WebGate/wsdl')
+
 
 class BaseGateway(object):
     def send(self):
@@ -7,9 +11,24 @@ class BaseGateway(object):
         raise NotImplementedError()
 
 
-class PayirGateway(BaseGateway):
-    def send(self):
-        pass
+class ZarinpalGateway(BaseGateway):
+    def send(user, pk):
+        result = zarinpal_client.service.PaymentRequest(
+            user['merchant'], user['amount'], user['description'], user['email'], user['mobile'],
+            user['CallbackURL'] + str(pk))
 
-    def verify(self):
-        pass
+        if result.Status == 100:
+            return ('https://sandbox.zarinpal.com/pg/StartPay/' + str(result.Authority))
+        else:
+            return ('Error code: ' + str(result.Status))
+
+    def verify(user):
+        result = zarinpal_client.service.PaymentVerificationWithExtra(user['merchant'], user['authority'],
+                                                                      user['amount'])
+
+        if result.Status == 100:
+            return True
+        elif result.Status == 101:
+            return ('Transaction submitted : ' + str(result.Status))
+        else:
+            return ('Transaction failed.\nStatus: ' + str(result.Status))
