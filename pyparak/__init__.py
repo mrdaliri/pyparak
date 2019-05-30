910368379f33cb9f17deb25e61da1d64572dec8e
AmirHosseinFayyazian <amirh.fayyazian95@gmail.com>
2019-05-30 04:43:59 +0430

    Edited url zarinpal, added _api_url

--- pyparak/__init__.py
+++ pyparak/__init__.py
@@ -8,11 +8,14 @@ class BaseGateway(object):
     def verify(self):
         raise NotImplementedError()
 
+    def confirm(self):
+        raise NotImplementedError()
+
 
 class ZarinpalGateway(BaseGateway):
-
-    def __init__(self):
-        self.client = Client('https://sandbox.zarinpal.com/pg/services/WebGate/wsdl')
+    def __init__(self, debug=False):
+        self.debug = debug
+        self.client = Client(self._api_url('https://zarinpal.com/pg/services/WebGate/wsdl'))
 
     def send(self, data):
         result = self.client.service.PaymentRequest(data['merchant'], data['amount'], data['description'],
@@ -20,16 +23,16 @@ class ZarinpalGateway(BaseGateway):
 
         if result.Status == 100 and len(result.Authority) == 36:
             return {'authority': result.Authority,
-                    'url': 'https://sandbox.zarinpal.com/pg/StartPay/{0}'.format(result.Authority)}
+                    'url': self._api_url('https://zarinpal.com/pg/StartPay/{0}').format(result.Authority)}
         else:
-            raise Exception(self.error_code(result.Status))
+            raise Exception({"code": result.Status, "detail": self._translate_error(result.Status)})
 
     def verify(self, data):
         result = self.client.service.PaymentVerificationWithExtra(data['merchant'], data['authority'], data['amount'])
         if result.Status == 100:
             return {'status': result.Status, 'reference_id': result.RefID}
         else:
-            raise Exception(self.error_code(result.Status))
+            raise Exception({"code": result.Status, "detail": self._translate_error(result.Status)})
 
     def confirm(self, data):
         if data['query_params']['Status'] == 'OK':
@@ -38,15 +41,21 @@ class ZarinpalGateway(BaseGateway):
             return self.verify(verify_data)
 
         else:
-            raise Exception(self.error_code(-22))
+            raise Exception({"code": -22, "detail": self._translate_error(-22)})
 
-    def error_code(self, code):
+    def _translate_error(self, code):
         switcher = {
             101: "Transaction submitted.",
             -42: "Time out validation",
             -22: "Transaction canceled by user",
             -11: "request not found",
             -3: "Payment value error",
+            -2:"Invalid ip address or merchant",
             -1: "ّInformation send is incomplete",
         }
         return switcher.get(code, "error not found")
+
+    def _api_url(self, url):
+        if self.debug:
+            return url.replace("zarinpal.com", "sandbox.zarinpal.com")
+        return url
